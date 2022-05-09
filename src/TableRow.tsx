f96319b06d08e3b40e87e1f1b2622627cd6677c0
WrittenWoods <consideredlily@gmail.com>
2022-05-09 12:19:28 -0400

    bugfix: tags field is now a string in state

--- src/TableRow.tsx
+++ src/TableRow.tsx
@@ -5,7 +5,7 @@ function TableRow({ appState, rowData, rowIndex }) {
   const [loadedData, setLoadedData, userInputs] = appState
 
   const [title, setTitle] = useState(rowData.title)
-  const [tags, setTags] = useState(rowData.tags)
+  const [tagsText, setTagsText] = useState(rowData.tags.join(", "))
   const [runtime, setRuntime] = useState(rowData.runtime)
   const [interest, setInterest] = useState(rowData.interest)
   const [editRow, toggleEditRow] = useState(false)
@@ -13,7 +13,7 @@ function TableRow({ appState, rowData, rowIndex }) {
   function handleEditButtonClick() {
     if (editRow) {
       let updated = [...loadedData]
-      updated[rowIndex] = { title: title, tags: tags, runtime: runtime, interest: interest }
+      updated[rowIndex] = { title: title, tags: tagsText.split(",").map(x => x.trim()), runtime: runtime, interest: interest }
       setLoadedData(updated)
     }
     toggleEditRow(!editRow)
@@ -32,7 +32,7 @@ function TableRow({ appState, rowData, rowIndex }) {
           <td><button onClick={() => handleDeleteButtonClick()}>delete</button></td>
           <td><button onClick={() => handleEditButtonClick()}>save</button></td>
           <td><textarea value={title} onChange={(e) => setTitle(e.target.value)} /></td>
-          <td><textarea value={tags.join(", ")} onChange={(e) => setTags(e.target.value.split(",").map(x => x.trim()))}/></td>
+          <td><textarea value={tagsText} onChange={(e) => setTagsText(e.target.value)} /></td>
           <td><textarea value={runtime} onChange={(e) => setRuntime(Number(e.target.value))} /></td>
           <td><textarea value={interest} onChange={(e) => setInterest(Number(e.target.value))} /></td>
         </tr>
@@ -43,7 +43,7 @@ function TableRow({ appState, rowData, rowIndex }) {
           <td><button onClick={() => handleDeleteButtonClick()}>delete</button></td>
           <td><button onClick={() => handleEditButtonClick()}>edit</button></td>
           <td>{title}</td>
-          <td>{tags.join(", ")}</td>
+          <td>{tagsText}</td>
           <td>{runtime}</td>
           <td>{interest}</td>
         </tr>
